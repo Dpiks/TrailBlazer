8d167fedbe4fa187c0adb5b7f196ccb6847e5d8f
Deepika MUSUVATHY <Deepika MUSUVATHY>
2017-03-18 18:59:09 -0400

    parks table ready with 38 records

--- server1.js
+++ server1.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var handlebars = require("express-handlebars");
 var request = require("request");
 var unirest = require('unirest');
-var axios=require('axios');
+var axios = require('axios');
 
 var app = express();
 
@@ -26,34 +26,54 @@ var db = require("./models");
 
 var port = process.env.PORT || 3000;
 
+function parkFilter(parks) {
+    return parks.filter(function(p, i, arr) {
+        return p.latLong !== '';
+    });
+};
+
+
+function NPSaxios(req, res) {
+    axios.get('https://developer.nps.gov/api/v0/parks?', {
+            headers: {
+                'Authorization': 'FF6C5C39-6EA5-47E1-B07C-C70339D68963'
+            }
+        })
+        .then(function(result) {
+            var filter = result.data.data;
+            console.log(filter.length);
+            for (i = 0; i < filter.length; i++) { 
+                console.log(filter[i].parkCode);                   
+                if (filter[i].latLong.length > 3) {
+                    var latLong = filter[i].latLong.split(",");
+                    console.log("latLong" + latLong);
+                    var latitude = latLong[0].split(":");
+                    latitude = latitude[1];
+                    var longitude = latLong[1].split(":");
+                    longitude = longitude[1];
+                    console.log(latitude,longitude);
+
+                    db.Park.create({
+                        park_id: filter[i].parkCode,
+                        park_name: filter[i].fullName,
+                        description: filter[i].description,
+                        latitude: latitude,
+                        longitude: longitude,
+                        state: filter[i].states,
+                        directions_info: filter[i].directionsInfo,
+                        url: filter[i].url,
+                        directions_url: filter[i].directionsUrl,
+                        weather_info: filter[i].weatherInfo
+                    })
+                }
+            }
+
+        });
+};
+
 db.sequelize.sync({ force: true }).then(function() {
     app.listen(port, function() {
         console.log("App listening on PORT " + port);
-        console.log(data.length);
-
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].latLong.length > 3) {
-                var latLong = data[i].latLong.split(",");
-
-                console.log("latLong" + latLong);
-                var latitude = latLong[0].split(":");
-                latitude = latitude[1];
-                var longitude = latLong[1].split(":");
-                longitude = longitude[1];
-
-                db.Park.create({
-                    park_id: data[i].parkCode,
-                    park_name: data[i].fullName,
-                    description: data[i].description,
-                    latitude: latitude,
-                    longitude: longitude,
-                    state: data[i].states,
-                    directions_info: data[i].directionsInfo,
-                    url: data[i].url,
-                    directions_url: data[i].directionsUrl,
-                    weather_info: data[i].weatherInfo
-                })
-            }
-        }
+        NPSaxios();
     });
 });
