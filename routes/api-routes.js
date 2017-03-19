842bea62c2288707a0a46c362895ebe6b294ae7d
Deepika MUSUVATHY <Deepika MUSUVATHY>
2017-03-18 20:13:07 -0400

    Code to get trails based on the given latitude, longitude is added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var unirest = require('unirest');
 
 
 module.exports = function(app) {
@@ -15,4 +16,18 @@ module.exports = function(app) {
         });
 
     });
+
+    app.get("/api/:latitude/:longitude", function(req, res) {
+    	var latitude=req.params.latitude;
+    	var longitude=req.params.longitude;
+        unirest.get("https://trailapi-trailapi.p.mashape.com/?lat=" + latitude + "&limit=25&lon=" + longitude + "&q[activities_activity_type_name_eq]=hiking&radius=50")
+            .header("X-Mashape-Key", "1EUZc9Yh0Dmsh3NULqLjzLCBf7rsp1iedcgjsnE14nUri24ZVA")
+            .header("Accept", "text/plain")
+            .end(function(result) {                
+                res.json(result.body);
+            });      
+
+    });
+
+
 }
